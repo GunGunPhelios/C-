0e303b591b13cae4f9df02dc3f4840ec39627baf
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-02-17 11:23:54 +0100

    Add files via upload

---
+++ 3as feladat/3as feladat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3as_feladat
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] nevek = { "Anna", "Elemér", "Béla", "John", "Jenna", "Kinga" };
+
+            for (int i = 0; i < nevek.Length; i++)
+            {
+                if (nevek[i] == "Anna")
+                {
+                    nevek[i] = "Kata";
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", nevek));
+        }
+    }
+}
+
+
