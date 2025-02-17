a4b637ca2e5f53a4105e516c7c9593d28af62362
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-02-17 10:38:11 +0100

    Add files via upload

---
+++ 2es feladat/2es feladat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2es_feladat
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<int> szamok = new List<int>();
+            int szamokSzama = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Kérlek, adj meg egy számot: ");
+                int szam = Convert.ToInt32(Console.ReadLine());
+                szam += 2;
+                if (szam > 20 && szam < 100)
+                {
+                    szamok.Add(szam);
+                    szamokSzama++;
+                    Console.WriteLine($"{szamokSzama}. szám hozzáadva: {szam}");
+                }
+            }
+
+            Console.WriteLine($"A lista hossza: {szamokSzama}");
+        }
+    }
+
+}
---
+++ Dolgozatgyakorlat/Dolgozatgyakorlat/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozatgyakorlat
+
+/*
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+
+
+            List<int> paratlan = new List<int>();
+            while (paratlan.Count < 32) {
+
+                int szam = rnd.Next(1, 50);
+                if (szam % 2 == 0)
+                {
+                    Console.WriteLine($"A szám páros {szam}.");
+                }
+                else
+                {
+                    Console.WriteLine($"A szám páratlan {szam}.");
+                    paratlan.Add(szam);
+
+                }
+                }
+            Console.WriteLine(string.Join(",", paratlan));
+            Console.WriteLine($"A lista hossza: {paratlan.Count}");
+            Console.ReadKey();
+        }
+           
+
+        }
+    }
+
+        Console.ReadKey();
+*/
+
+/* 2es feladat */
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> szamok = new List<int>();
+        int szamokSzama = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write("Kérlek, adj meg egy számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            szam += 2;
+            if (szam > 20 && szam < 100)
+            {
+                szamok.Add(szam);
+                szamokSzama++;
+                Console.WriteLine($"{szamokSzama}. szám hozzáadva: {szam}");
+            }
+        }
+
+        Console.WriteLine($"A lista hossza: {szamokSzama}");
+    }
+}
+
+
+
+
+
+}
