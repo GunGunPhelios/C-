6f66979e4c6a60178072222748a410cae4c6e5eb
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-02-18 13:28:11 +0100

    Add files via upload

---
+++ gümölcsös/gümölcsös/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gümölcsös
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<string> kosar = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Milyen gyümölcsöt szeretnél a kosárhoz adni?");
+                string gyumolcs = Console.ReadLine();
+                kosar.Add(gyumolcs);
+                if(kosar.Count >= 10)
+                {
+                 
+                    Console.WriteLine("Megtelt a kosarad. Már 10 gyümölcsöd van. Ha mégis hozzá szeretnál adni, akkor cserélj le egy gyümölcsöt");
+                break;
+                }
+            }
+            Console.WriteLine(string.Join(",", kosar));
+            Console.WriteLine("Melyiket sZERETNÉD ELTÁVOLITANI?");
+            string modosít = Console.ReadLine();
+            for (int i = 0; i < kosar.Count; i++)
+
+            {
+                if (kosar[i] == modosít) 
+                {
+                    Console.WriteLine("Mire cserélnéd le?");
+                    string csere= Console.ReadLine();
+                    kosar[i] = csere;
+                }
+            }
+            Console.WriteLine(string.Join(",", kosar));
+            Console.ReadKey();
+        }
+            */
+
+
+            Console.ReadKey();
+
+        }
+}
