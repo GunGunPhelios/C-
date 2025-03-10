13f2387c8ef80f56891d76e31ebee44815c553db
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-03-10 11:05:58 +0100

    Add files via upload

---
+++ LoL/ConsoleApp6/regisztráció.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class regisztráció
+    {
+        public string nev;
+        public string felnev;
+        public string email;
+        public string jelszo;
+        public DateTime szuldatum;
+
+        public regisztráció(string nev, string felnev, string email, string jelszo, string szuldatum)
+        { 
+            this.nev = nev;
+            this.felnev = felnev;
+            this.email = email;
+            this.jelszo = jelszo;
+            this.szuldatum = Convert.ToDateTime(szuldatum);
+        }
+    }
+}
