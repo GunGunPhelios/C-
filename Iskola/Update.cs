e82d0a79da3509e65fee55926fd6ee98dbad5f25
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-03-13 09:03:52 +0100

    Add files via upload

---
+++ Iskola/Update.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskola
+{
+    internal class Update
+    {
+        public static string connection = "localhost=server;database=iskola;user=root;password=;";
+
+        public static void AdatModosit(int diak id, string dveznev, string keresztnev, string osztaly)
+        {
+            string query= "UPDATE diak SET dveznev= @dveznev, dkeresztnev= @dkeresztnev, osztaly= @osztaly WHERE diak_id=@id "
+        }   using (MysqlConnection cmd = new MysqlConnection(Query,conn)){
+            cmd.Parameters.AddWithValue(@ID,ID);
+            cmd.Parameters.AddWithValue(@dveznev, dveznev);
+        cmd.Parameters.AddWithValue(@dkernev, dkernev);
+        cmd.Parameters.AddWithValue(@dosztaly, dosztaly);
+        cmd.Parameters.AddWithValue(@ID, ID);
+        cmd.Parameters.AddWithValue(@ID, ID);
+
+
+            }
+}
