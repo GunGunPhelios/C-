1668907bac29751cfeb3a3844ebc5fecde5390b4
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-03-18 13:20:21 +0100

    Update ellenorzes.cs

--- Bank/ellenorzes.cs
+++ Bank/ellenorzes.cs
@@ -1,40 +1,55 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bank
 {
-    internal class ellenorzes
+    internal class Ellenorzes
     {
-        public static string connect = "server=localhost;database=bank;user=root;password=;";
-        public static AdatEllenorzes(string tulajdonos_nev, int azonosito, int pinkod)
-        {
-            string query= "SELECT tulajdonos_nev, azonosito,pinkot FROM szamla_tulajdonosok WHERE tulajdonos_nev"
-        using (MySqlConnection conn= new MySqlConnection(connect))
-            {
+        public static string connect = "server=localhost;database = bank;user = root; password = ;";
+
+       
+        public static bool  AdatEllenorzes(string tulajdonos_nev,int azonosito,int pinkod) {
+
+            Console.WriteLine("Adja meg a nevét: ");
+            tulajdonos_nev = Console.ReadLine();
+            Console.WriteLine("Adja meg az azonositoját: ");
+            azonosito = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a pinkodót:");
+            pinkod = int.Parse(Console.ReadLine());
+
+            bool sikeres = false;
+
+            using (MySqlConnection conn = new MySqlConnection(connect)) {
                 conn.Open();
-                using(MySqlCommand cmd= new MySqlCommand(query, conn))
-                {
+                string query = "SELECT tulajdonos_nev,azonosito,pinkod FROM szamla_tulajdonosok WHERE tulajdonos_nev = @tulajdonos_nev AND azonosito = @azonosito AND pinkod = @pinkod";
+
+               
+                using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
                     cmd.Parameters.AddWithValue("@tulajdonos_nev", tulajdonos_nev);
                     cmd.Parameters.AddWithValue("@azonosito", azonosito);
                     cmd.Parameters.AddWithValue("@pinkod", pinkod);
-                    cmd.ExecuteNonQuery();
-                }
-                using(MySqlDataReader reader= new MySqlDataReader(cmd,conn))
-                {
-                    reader.Read();
-                    sikeres=true;
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+
+                            sikeres = true;
+                        }
+                       
+                    }
+                   
 
                 }
+               
+
             }
-                }
-        return sikeres;
-internal static bool AdatEllenorzes(string tulajdonos_neve, int azonositas, int pinkodmegad)
-        {
-            throw new NotImplementedException();
+            return sikeres;
         }
     }
 }
