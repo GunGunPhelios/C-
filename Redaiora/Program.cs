57fe10c3bd0e3a2aea5d054ffec83e5a6a23528e
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-04-04 13:04:14 +0200

    Add files via upload

---
+++ Redaiora/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Redaiora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            jatek j1 = new();
+
+            j1.Termek = "Uno";
+            j1.Kategoria = "kártyajáték";
+            j1.Ar = 6890;
+            j1.Keszlet = 500;
+
+            Console.WriteLine(j1.Termek);
+
+            Console.WriteLine(j1);
+
+
+            List<jatek> jatekok = new();
+            {
+                new jatek("Uno", "Kártyajáték", 6890, 500);
+                new jatek("Monopoly", "Társasjáték", 9990, 100);
+
+            }
+            Console.WriteLine("Raktár tartalma:");
+            jatekok.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Foreachel végig iterálva az elemeken:");
+            jatekok.ForEach(Console.WriteLine);
+
+            Console.WriteLine("Társasjátékok:");
+            var tarsasjatekok=jatekok.Where(j => j.Kategoria == "Társasjáték").ToList();
+            tarsasjatekok.ForEach(jatekok => Console.WriteLine(jatekok.Termek));
+            Console.WriteLine("Kártyajátékok");
+            var kartyajatekok=jatekok.Where(j=>j.Kategoria=="Kártyajáték").ToList();
+            tarsasjatekok.ForEach(jatekok => Console.WriteLine("/t-" + jatekok.Termek));
+
+            Console.WriteLine("n Legdrágább játék:");
+            var legdragabb= jatekok.OrderByDescending(j=> j.Ar).First();
+            Console.WriteLine(legdragabb.Termek+ " " + legdragabb.Ar + "HUF");
+
+            Console.WriteLine("/n Kevesebb drab van raktáron mint 200 db:");
+            var kevesebb=jatekok.Where(j=> j.Keszlet> 200).ToList();
+            tarsasjatekok.ForEach(j => Console.WriteLine("/t-" + j.Termek + " " + j.Keszlet + "db"));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Redaiora/jatek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redaiora
+{
+    internal class jatek
+    {
+        public string Termek { get; set; }
+        public string Kategoria { get; set; }
+
+        public int Ar { get; set; }
+
+        public int Keszlet { get; set; }
+
+
+        public jatek(string termek, string kategoria, int ar, int keszlet)
+        {
+            Termek = termek;
+            Kategoria = kategoria;
+            Ar = ar;
+            Keszlet = keszlet;
+        }
+
+        public jatek()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Termek} {Kategoria} {Ar} {Keszlet}";
+
+        }
+    }
+}
