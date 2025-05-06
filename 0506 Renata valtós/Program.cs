0fdda65d1209216684c2f0c4b15bbb25f15f906e
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2025-05-06 12:39:47 +0200

    Add files via upload

---
+++ 0506 Renata valtós/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cimletek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cimletek = { 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5 };
+            Console.WriteLine("Adj meg egy összeget:");
+            int osszeg = Convert.ToInt32(Console.ReadLine());
+            while (osszeg > 0)
+            {
+                for (int i = 0; i < cimletek.Length; i++) {
+                    int eredmeny = osszeg / cimletek[i];
+                    if (eredmeny > 0) {
+                        osszeg -= eredmeny * cimletek[i];
+                        Console.WriteLine($"{eredmeny} db {cimletek[i]}");
+
+                    }
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
